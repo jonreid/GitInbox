eb0eb714dffec73912bf21aabd05514275589a63
Jon Reid <jon@qualitycoding.org>
2023-11-19 10:28:07 -0800

    e - New Xcode project Interface: SwiftUI; Language: Swift: Unit Tests on.

---
+++ GitInbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitInbox
+//
+//  Created by Jon Reid on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by Jon Reid on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
