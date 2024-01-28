661f9d5758d45a66bb3e8db22c0d87bd7304e0b9
Jon Reid <jon@qualitycoding.org>
2024-01-28 09:59:12 -0800

    e - install certificate in keychain

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -5,8 +5,8 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/Certificates.p12 provisioning/Certificates.p12.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/GitInbox_Distribution.mobileprovision provisioning/GitInbox_Distribution.mobileprovision.gpg
 
+# Move provisioning profile into place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
 echo "-------------"
 echo "List profiles:"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
@@ -17,10 +17,11 @@ echo "-------------"
 echo "List profiles:"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
-# security create-keychain -p "" build.keychain
-# security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
+# Move certificate into keychain
+security create-keychain -p "" build.keychain
+security import provisioning/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
 
-# security list-keychains -s ~/Library/Keychains/build.keychain
-# security default-keychain -s ~/Library/Keychains/build.keychain
-# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
