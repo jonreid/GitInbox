459dd40448d67e07f82e3b443cb6fd280e98ad80
Jon Reid <jon@qualitycoding.org>
2024-02-12 19:09:19 -0800

    e - introduce explaining variable for keychain password

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -4,6 +4,8 @@
 CERTIFICATE='provisioning/Certificates.p12'
 PROVISIONING='provisioning/GitInbox_Distribution.mobileprovision'
 
+KEYCHAIN_PASSWORD=''
+
 echo 'Decrypt the files'
 # --batch to prevent interactive command, --yes to assume "yes" for questions
 gpg --decrypt --quiet --batch --yes --passphrase="$PROVISIONING_PASSWORD" --output "$CERTIFICATE" "$CERTIFICATE".gpg
@@ -16,10 +18,10 @@ echo "List profiles:"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 echo 'Move certificate into keychain'
-security create-keychain -p "" build.keychain
+security create-keychain -p "$KEYCHAIN_PASSWORD" build.keychain
 security import "$CERTIFICATE" -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
 security list-keychains -s ~/Library/Keychains/build.keychain
 
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PASSWORD" ~/Library/Keychains/build.keychain
