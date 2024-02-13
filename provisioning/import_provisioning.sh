344585cf62617f9d4b15e7ccd8c8b1165cb159cc
Jon Reid <jon@qualitycoding.org>
2024-02-12 18:51:32 -0800

    e - change comments to echo statements

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -1,31 +1,25 @@
 @ -1,27 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 CERTIFICATE='provisioning/Certificates.p12'
 PROVISIONING='provisioning/GitInbox_Distribution.mobileprovision'
 
-# Decrypt the files
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output "$CERTIFICATE" "$CERTIFICATE".gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output "$PROVISIONING" "$PROVISIONING".gpg
+echo 'Decrypt the files'
+# --batch to prevent interactive command, --yes to assume "yes" for questions
+gpg --decrypt --quiet --batch --yes --passphrase="$PROVISIONING_PASSWORD" --output "$CERTIFICATE" "$CERTIFICATE".gpg
+gpg --decrypt --quiet --batch --yes --passphrase="$PROVISIONING_PASSWORD" --output "$PROVISIONING" "$PROVISIONING".gpg
 
-# Move provisioning profile into place
+echo 'Move provisioning profile into place'
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "-------------"
-echo "List profiles:"
-ls ~/Library/MobileDevice/Provisioning\ Profiles/
-echo "-------------"
-echo "Move profiles"
 cp "$PROVISIONING" ~/Library/MobileDevice/Provisioning\ Profiles/
-echo "-------------"
 echo "List profiles:"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
-# Move certificate into keychain
+echo 'Move certificate into keychain'
 security create-keychain -p "" build.keychain
 security import "$CERTIFICATE" -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
-
 security list-keychains -s ~/Library/Keychains/build.keychain
+
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
