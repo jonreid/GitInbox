3dd6ef02a797a76df37a2404c49bb35a5d2906be
Jon Reid <jon@qualitycoding.org>
2024-02-12 21:41:02 -0800

    e - turn off code signing for test build

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15"  | xcbeautify --renderer github-actions && exit ${PIPESTATUS[0]}
+xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15" CODE_SIGNING_ALLOWED="NO" | xcbeautify --renderer github-actions && exit ${PIPESTATUS[0]}
