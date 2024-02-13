0204f66c1427f4fb8d84c0c4268ede4d8459e7e2
Jon Reid <jon@qualitycoding.org>
2024-02-12 21:56:46 -0800

    e - use xcbeautify for build_archive.sh

--- build_archive.sh
+++ build_archive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xcodebuild archive -project GitInbox.xcodeproj -scheme GitInbox -sdk iphoneos -configuration Release -derivedDataPath DerivedData -archivePath DerivedData/Archive/GitOut
+xcodebuild archive -project GitInbox.xcodeproj -scheme GitInbox -sdk iphoneos -configuration Release -derivedDataPath DerivedData -archivePath DerivedData/Archive/GitOut | xcbeautify --renderer github-actions
 xcodebuild -exportArchive -archivePath DerivedData/Archive/GitOut.xcarchive -exportOptionsPlist provisioning/App-Store.plist -exportPath DerivedData/ipa
