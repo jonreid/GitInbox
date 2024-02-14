784816a74c199d45ed13d8ab265c85ea5eeec37c
Jon Reid <jon@qualitycoding.org>
2024-02-13 18:32:11 -0800

    e - extract variables to make run_tests.sh easier to configure

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-set -o pipefail && xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15" CODE_SIGNING_ALLOWED="NO" | tee xcodebuild.log | xcbeautify --renderer github-actions
+PROJECT='GitInbox.xcodeproj'
+SCHEME='GitInbox'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+
+xcodebuild -showdestinations -project $PROJECT -scheme $SCHEME
+set -o pipefail && \
+  xcodebuild test -project $PROJECT -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' \
+  | tee xcodebuild.log \
+  | xcbeautify --renderer github-actions
