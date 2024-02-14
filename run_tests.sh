8fbc1c12ff43bdcc88ce4488d9a9fdce6ac0dcb2
Jon Reid <jon@qualitycoding.org>
2024-02-13 18:53:16 -0800

    e - no xcbeautify, no project name, control result bundle path

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-PROJECT='GitInbox.xcodeproj'
 SCHEME='GitInbox'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
 
-xcodebuild -showdestinations -project $PROJECT -scheme $SCHEME
-set -o pipefail && \
-  xcodebuild test -project $PROJECT -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' \
-  | tee xcodebuild.log \
-  | xcbeautify --renderer github-actions
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -resultBundlePath TestResults CODE_SIGNING_ALLOWED='NO'
