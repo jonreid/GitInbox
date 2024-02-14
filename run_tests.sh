34a33f30d659412c7f97f1ab2816e521be179a42
Jon Reid <jon@qualitycoding.org>
2024-02-13 18:14:16 -0800

    e - write raw xcodebuild log as well as beautifying

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15" CODE_SIGNING_ALLOWED="NO" | xcbeautify --renderer github-actions && exit ${PIPESTATUS[0]}
+set -o pipefail && xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15" CODE_SIGNING_ALLOWED="NO" | tee xcodebuild.log | xcbeautify --renderer github-actions
