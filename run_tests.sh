e9ff5468f4e7b89f2c1dbac473e65127f4a0abfb
Jon Reid <jon@qualitycoding.org>
2024-02-14 21:37:38 -0800

    e - stop writing xcresult to special path

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 SCHEME='GitInbox'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
 
-xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -resultBundlePath TestResults CODE_SIGNING_ALLOWED='NO'
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
